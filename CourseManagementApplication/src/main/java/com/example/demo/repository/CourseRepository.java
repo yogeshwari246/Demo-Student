28529a4e8d9bf749885e800e9a25f3dc2f6dd83e
Yogeshwari Rathod <rathodyogeshwari76@gmail.com>
2026-06-20 11:44:00 +0530

    Delete CourseManagementApplication directory

--- CourseManagementApplication/src/main/java/com/example/demo/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-
-}
